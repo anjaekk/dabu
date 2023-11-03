4a970dfdca4d61304665f1a6ac5c13633f10c088
An jaegyeong <anjaekk@gmail.com>
2023-11-03 17:29:38 +0900

    initial commit

---
+++ src/main/java/b172/challenging/ChallengingApplication.java
@@ -0,0 +1,13 @@
+package b172.challenging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/b172/challenging/ChallengingApplicationTests.java
@@ -0,0 +1,13 @@
+package b172.challenging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
