b4ec64adb85679bedf14eb70b2442fa2c8d7e8ca
Chang. LEE <43161771+LCJJam@users.noreply.github.com>
2024-01-09 13:36:04 +0900

    fix: SecurityConfig.java

--- src/main/java/b172/challenging/auth/config/SecurityConfig.java
+++ src/main/java/b172/challenging/auth/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
